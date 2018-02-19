0cf2aaa56eea33abaf8417801d7329b106a8f352
Josepcampos <32305641+Josepcampos@users.noreply.github.com>
2018-02-19 17:15:26 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -34,28 +34,7 @@ xhttp.send();
 //FUNCIÓN PARSEAR
 function gestionarXml(dadesXml){
   var xmlDoc = dadesXml.responseXML;
-	/*
-  document.getElementById("h21").innerHTML = xmlDoc.getElementsByTagName("number")[0].childNodes[0].nodeValue;
-  document.getElementById("q1").innerHTML = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
-  document.getElementById("h22").innerHTML = xmlDoc.getElementsByTagName("number")[1].childNodes[0].nodeValue;
-  document.getElementById("q2").innerHTML = xmlDoc.getElementsByTagName("title")[1].childNodes[0].nodeValue;
-  document.getElementById("h23").innerHTML = xmlDoc.getElementsByTagName("number")[2].childNodes[0].nodeValue;
-  document.getElementById("q3").innerHTML = xmlDoc.getElementsByTagName("title")[2].childNodes[0].nodeValue;
-  document.getElementById("h24").innerHTML = xmlDoc.getElementsByTagName("number")[3].childNodes[0].nodeValue;
-  document.getElementById("q4").innerHTML = xmlDoc.getElementsByTagName("title")[3].childNodes[0].nodeValue;
-  document.getElementById("h25").innerHTML = xmlDoc.getElementsByTagName("number")[4].childNodes[0].nodeValue;
-  document.getElementById("q5").innerHTML = xmlDoc.getElementsByTagName("title")[4].childNodes[0].nodeValue;
-  document.getElementById("h26").innerHTML = xmlDoc.getElementsByTagName("number")[5].childNodes[0].nodeValue;
-  document.getElementById("q6").innerHTML = xmlDoc.getElementsByTagName("title")[5].childNodes[0].nodeValue;
-  document.getElementById("h27").innerHTML = xmlDoc.getElementsByTagName("number")[6].childNodes[0].nodeValue;
-  document.getElementById("q7").innerHTML = xmlDoc.getElementsByTagName("title")[6].childNodes[0].nodeValue;
-  document.getElementById("h28").innerHTML = xmlDoc.getElementsByTagName("number")[7].childNodes[0].nodeValue;
-  document.getElementById("q8").innerHTML = xmlDoc.getElementsByTagName("title")[7].childNodes[0].nodeValue;
-  document.getElementById("h29").innerHTML = xmlDoc.getElementsByTagName("number")[8].childNodes[0].nodeValue;
-  document.getElementById("q9").innerHTML = xmlDoc.getElementsByTagName("title")[8].childNodes[0].nodeValue;
-  document.getElementById("h210").innerHTML = xmlDoc.getElementsByTagName("number")[9].childNodes[0].nodeValue;
-  document.getElementById("q10").innerHTML = xmlDoc.getElementsByTagName("title")[9].childNodes[0].nodeValue;
-  */
+
 	for (var i=0;i<10;i++) {
 		document.getElementsByTagName("h2")[i].innerHTML = xmlDoc.getElementsByTagName("number")[i].innerHTML;
  		document.getElementsByTagName("p")[i].innerHTML = xmlDoc.getElementsByTagName("title")[i].innerHTML
